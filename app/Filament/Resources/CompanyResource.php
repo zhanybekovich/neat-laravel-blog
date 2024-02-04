b34b615cb101bb87877da9940a59479c35050b7f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-02-04 12:59:46 +0600

    Add company resource

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Filament\Resources\CompanyResource\RelationManagers;
+use App\Models\Company;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog-8-tooth';
+
+    protected static ?string $pluralLabel = 'Company';
+
+    protected static ?string $navigationGroup = 'General Settings';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\Textarea::make('description')
+                        ->maxLength(65535)
+                        ->columnSpanFull(),
+                    Forms\Components\FileUpload::make('logo')
+                        ->image()
+                        ->imageEditor(),
+                    Forms\Components\Textarea::make('address')
+                        ->required()
+                        ->maxLength(65535)
+                        ->columnSpanFull(),
+                    Forms\Components\TextInput::make('phone')
+                        ->tel()
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('email')
+                        ->email()
+                        ->required()
+                        ->maxLength(255),
+                ])
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('logo'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('email'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+}
